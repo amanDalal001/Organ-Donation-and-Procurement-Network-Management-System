684afd82c4fc68fa58223c69b5bb51633b14a85b
AMAN <amandalal56386@gmail.com>
2022-11-20 13:42:47 +0530

    20-11-22 login

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const port = 3000;
+
+const Users = [{
+    username: "aman",
+    password: "aman123"
+}]
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+// app.post('/', (req, res) => {
+//     const body = req.body;
+//     const username  = body.username;
+//     const password = body.password;
+
+    
+//     console.log(username+" "+ password);
+// });
+
+app.get('/login',(req, res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.post('/login',(req, res)=>{
+    const body = req.body;
+    const username  = body.username;
+    const password = body.password;
+
+    Users.forEach((user)=>{
+        if(user.username == username && user.password == password)
+        {
+            res.redirect('/login');
+        }
+    } )
+    console.log(username+" "+ password);
+});
+
+app.listen(port);
